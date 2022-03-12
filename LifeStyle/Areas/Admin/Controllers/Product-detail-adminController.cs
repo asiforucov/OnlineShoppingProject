6de9fe9b0380af90af33455ea7d80af131a420fb
Asif <92951624+asiforucov@users.noreply.github.com>
2022-03-12 18:04:17 +0400

    admin panel view update 1

---
+++ LifeStyle/Areas/Admin/Controllers/Product-detail-adminController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.Interfaces;
+
+namespace LifeStyle.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class Product_detail_adminController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public Product_detail_adminController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        public async Task<IActionResult> Index(int id)
+        {
+            var product = await _productService.Get(id);
+            return View(product);
+        }
+    }
+}
--- LifeStyle/Controllers/Product-detailController.cs
+++ LifeStyle/Controllers/Product-detailController.cs
@@ -4,16 +4,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Business.Interfaces;
+using Core;
 
 namespace LifeStyle.Controllers
 {
     public class Product_detailController : Controller
     {
+        private readonly IUnitOfWork _unitOfWork;
         private readonly IProductService _productService;
+        private readonly IProductCategoryService _productCategoryService;
+        private readonly IGenderCategoryService _genderCategoryService;
+        private readonly IProductColorService _productColorService;
+        private readonly IProductBrandService _productSizeService;
 
-        public Product_detailController(IProductService productService)
+        public Product_detailController(IUnitOfWork unitOfWork, IProductService productService, IProductCategoryService productCategoryService, IGenderCategoryService genderCategoryService, IProductColorService productColorService, IProductBrandService productSizeService)
         {
+            _unitOfWork = unitOfWork;
             _productService = productService;
+            _productCategoryService = productCategoryService;
+            _genderCategoryService = genderCategoryService;
+            _productColorService = productColorService;
+            _productSizeService = productSizeService;
         }
         public async Task<IActionResult> Index(int id)
         {
